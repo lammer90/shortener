0a61cc61203b33cfbfd95314a9838985c476a240
dmitriyplotnikov <lammer.90@yandex.ru>
2023-09-05 13:28:28 +0300

    iter1

--- internal/app/util/util.go
+++ internal/app/util/util.go
@@ -2,12 +2,13 @@ package util
 
 import (
 	"net/http"
+	"strings"
 )
 
 func CheckContentHeader(req *http.Request) bool {
 	val, ok := req.Header["Content-Type"]
 	if ok {
-		return "text/plain" == val[0]
+		return strings.Contains(val[0], "text/plain")
 	}
 	return false
 }
