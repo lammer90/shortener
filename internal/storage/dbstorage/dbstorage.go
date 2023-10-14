bd900dcbf03d5183a987695fc6ec90f86e8ebbcb
dmitriyplotnikov <lammer.90@yandex.ru>
2023-10-14 17:30:07 +0300

    iter11

--- internal/storage/dbstorage/dbstorage.go
+++ internal/storage/dbstorage/dbstorage.go
@@ -1,11 +1,51 @@
 package dbstorage
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
-func InitDB(driverName, dataSource string) *sql.DB {
-	db, err := sql.Open(driverName, dataSource)
+type dbStorage struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) dbStorage {
+	initDB(db)
+	return dbStorage{db: db}
+}
+
+func (d dbStorage) Save(key, value string) error {
+	_, err := d.db.ExecContext(context.Background(), `
+        INSERT INTO shorts
+        (short_url, original_url)
+        VALUES
+        ($1, $2);
+    `, key, value)
+	return err
+}
+
+func (d dbStorage) Find(key string) (string, bool, error) {
+	row := d.db.QueryRowContext(context.Background(), `
+        SELECT
+            s.original_url
+        FROM shorts s
+        WHERE
+            s.short_url = $1
+    `, key)
+
+	var value string
+	err := row.Scan(&value)
 	if err != nil {
-		panic(err)
+		return "", false, err
 	}
-	return db
+	return value, true, nil
+}
+
+func initDB(db *sql.DB) {
+	db.ExecContext(context.Background(), `
+        CREATE TABLE IF NOT EXISTS shorts (
+            short_url varchar PRIMARY KEY,
+            original_url varchar
+        )
+    `)
 }
