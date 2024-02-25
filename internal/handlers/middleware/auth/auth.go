038a98472cdf793439150d6d96782b06e7412416
dmitriyplotnikov <lammer.90@yandex.ru>
2024-02-25 19:53:45 +0300

    iter17

--- internal/handlers/middleware/auth/auth.go
+++ internal/handlers/middleware/auth/auth.go
@@ -1,12 +1,13 @@
 package auth
 
 import (
+	"net/http"
+
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/google/uuid"
 	"github.com/lammer90/shortener/internal/handlers"
 	"github.com/lammer90/shortener/internal/logger"
 	"github.com/lammer90/shortener/internal/userstorage"
-	"net/http"
 )
 
 type Authenticator struct {
